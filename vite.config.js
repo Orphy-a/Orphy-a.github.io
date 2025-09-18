af95792c2b94b68987bebc0bff055c01b211ea8e
jaeyoung0560 <jaeyoung0560@gmail.com>
2025-09-18 11:23:39 +0900

    gitpages 경로 수정

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Orphy-a.github.io/', // 여기를 실제 repository 이름으로 변경
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
