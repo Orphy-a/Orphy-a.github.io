415590e8052f4dd34e75b41e52d600412ac1eced
jaeyoung0560 <jaeyoung0560@gmail.com>
2025-09-18 10:31:35 +0900

    프로젝트 생성 및 화면 레이아웃

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className='header'>
+        <button className='menu-item'>캐릭터 검색</button>
+        <button className='menu-item'>검색</button>
+        <button className='menu-item'>검색</button>
+      </div>
+      <div className='content'>
+        <div className="main-logo">
+          로고 이미지
+        </div>
+        <div className="search">
+          <div className="search-area">
+            <div className="search-box">
+              <input></input>
+            </div>
+            <div className="search-btn">
+              <button>검색버튼</button>
+            </div>  
+          </div>
+          
+          <div className="search-recent">
+            최근 검색
+          </div>
+        </div>
+
+      </div>
+    </>  
+  );
+}
+
+export default App;
