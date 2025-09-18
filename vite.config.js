2695738f653535af887e04d2283762038edf321d
jaeyoung0560 <jaeyoung0560@gmail.com>
2025-09-18 10:57:02 +0900

    github pages 설정 수정

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Orphy-a.github.io/', // 여기를 실제 repository 이름으로 변경
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
 })
