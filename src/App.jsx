629c577c47ab6d1716aadde596dc12a88cf06380
jaeyoung0560 <jaeyoung0560@gmail.com>
2025-09-18 16:11:38 +0900

    메인 화면 구현 및 검색창 기능 구현 중

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,37 @@
-import { useState, useEffect } from 'react';
 import './App.css';
+import logo from "./assets/logo.png";
 
 function App() {
+  
+  const search = ()=>{
+    const auction =document.querySelector("#auction").value;
+    
+    console.log('auction:', auction);
+  }
+
   return (
     <>
       <div className='header'>
-        <button className='menu-item'>캐릭터 검색</button>
-        <button className='menu-item'>검색</button>
-        <button className='menu-item'>검색</button>
+        <button className='menu-item'>경매장 검색</button>
+        <button className='menu-item'>아이템 목록</button>
       </div>
       <div className='content'>
-        <div className="main-logo">
-          로고 이미지
+        <div className="main-logo"> 
+          <img src={logo} alt="메인 로고" />
         </div>
         <div className="search">
           <div className="search-area">
             <div className="search-box">
-              <input></input>
+              <input id='auction' placeholder='경매장 검색' onKeyDown={(e) => { if (e.key === 'Enter') { e.preventDefault(); search(); } }}></input>
             </div>
             <div className="search-btn">
-              <button>검색 버튼</button>
+              <button onClick={search}>검색 버튼</button>
             </div>  
           </div>
-          
           <div className="search-recent">
             최근 검색
           </div>
         </div>
-
       </div>
     </>  
   );
